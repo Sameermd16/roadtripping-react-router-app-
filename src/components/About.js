a8d4be216f12cd1ac9d45c545b6ea9088914e946
sameermd16 <sameerthemc@gmail.com>
2023-07-17 07:39:28 +0530

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function About() {
+    return (
+        <div>Nothing else to say about myself; Just another bum!</div>
+    )
+}
+
+export default About 
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Home() {
+    return (
+        <div>Sweet Home Alabama!</div>
+    )
+}
+
+export default Home
