8de12aa5c3993069dd093575fc399b92b632a8e6
sameermd16 <sameerthemc@gmail.com>
2023-07-25 20:29:00 +0530

    user data

--- src/components/api.js
+++ src/components/api.js
@@ -13,3 +13,11 @@ export async function getVans() {
     const data = await res.json()
     return data.vans 
 }
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login", 
+        { method: "post", body: JSON.stringify(creds)}
+    )
+    const data = await res.json()
+    return data 
+}
